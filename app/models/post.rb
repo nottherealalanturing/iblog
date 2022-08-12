0551b4fae13a656b3fb1857da6f0f6bbf4f9ab42
Assad Isah <nottherealalanturing@gmail.com>
2022-08-12 12:06:50 +0100

    fix: resolve errors with rubocop

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,13 @@
 class Post < ApplicationRecord
-
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, :likes_counter, numericality: { greater_than_or_equal_to: 0 }
-
 
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments
   has_many :likes
 
   after_save :update_posts_counter
-  
+
   def update_posts_counter
     author.update(posts_counter: author.posts.count)
   end
