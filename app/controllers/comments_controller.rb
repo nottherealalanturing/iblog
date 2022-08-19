badc5afc35cfcaed034b626f789f072f939ce425
Assad Isah <nottherealalanturing@gmail.com>
2022-08-19 15:52:36 +0100

    feat: add likes controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,25 @@
 class CommentsController < ApplicationController
   def new
+    @post = Comment.new
   end
 
   def create
+    @post = Post.find(params[:post_id])
+    new_comment = current_user.comments.new(
+      text: comment_params,
+      author_id: current_user.id,
+      post_id: @post.id
+    )
+    if new_comment.save
+      redirect_to "/users/#{@post.author_id}/posts/#{@post.id}", notice: 'Success!'
+    else
+      render :new, alert: 'Error occured!'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)[:text]
   end
 end
