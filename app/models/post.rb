d7c8f00b29434870e0604e619de4df36724400ec
Assad Isah <nottherealalanturing@gmail.com>
2022-08-19 10:53:40 +0100

    feat: add posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,4 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, :likes_counter, numericality: { greater_than_or_equal_to: 0 }
 
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments
@@ -13,6 +11,6 @@ class Post < ApplicationRecord
   end
 
   def last_five_comments
-    comments.includes(:post).order(created_at: :desc).limit(5)
+    comments.order(created_at: :DESC).limit(5)
   end
 end
